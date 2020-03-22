750f450d4137bd14ce05b03273e96922137ef44a
lambdaydoty <euphrates.tigris@gmail.com>
2020-03-22 03:30:34 +0000

    Add rippleAddress custom

--- customs.js
+++ customs.js
@@ -1,7 +1,13 @@
+const { RippleAPI } = require('ripple-lib')
 module.exports = [
   [
     /* name */ 'lowercase',
     /* callbackFn */ (v, req, attr) => v.toLowerCase() === v,
     /* errorMessage */ 'The :attribute is not in lower case',
+  ],
+  [
+    'rippleAddress',
+    (v, req, attr) => new RippleAPI().isValidAddress(v),
+    'The :attribute is not a ripple address',
   ],
 ]
